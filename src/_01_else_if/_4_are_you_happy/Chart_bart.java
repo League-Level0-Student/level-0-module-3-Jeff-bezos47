9bf8aa67bcf017c2d797bb9643becaa66a81bc8c
league <league@192.168.1.78>
2025-11-01 11:27:07 -0700

    asdfasdf

---
+++ src/_01_else_if/_4_are_you_happy/Chart_bart.java
@@ -0,0 +1,18 @@
+package _01_else_if._4_are_you_happy;
+
+import javax.swing.JOptionPane;
+
+public class Chart_bart {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String something = JOptionPane.showInputDialog("are you happy, yes or no, TELL ME");
+		if(something.equals("yes")) {
+			JOptionPane.showMessageDialog(null,"keep doing whatever you're doing");
+		}else if(something.equals("no")) {
+			
+			
+		}
+	}
+
+}
