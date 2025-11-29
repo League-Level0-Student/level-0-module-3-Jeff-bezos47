343f0758be9aa132f3376c4753b21c798d930db4
league <bobby>
2025-11-29 11:26:58 -0800

    asdfagsdfgsdfg

---
+++ src/_03_remainder/_1_intro_to_remainder/Lalalalala.java
@@ -0,0 +1,25 @@
+package _03_remainder._1_intro_to_remainder;
+
+public class Lalalalala {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i=1;  i < 9;  i++) {
+			 if(i == 4) {
+				 System.out.println(i + ", ");
+			 }
+			 else if(i == 8) {
+				 System.out.print("more");
+			 }
+			 else {	
+			System.out.print(i + " potato, ");
+			 }
+			 
+			
+		}
+	}
+
+}
+//1 potato, 2 potato, 3 potato, 4
+//
+//5 potato, 6 potato, 7 potato, more 
---
+++ src/_03_remainder/_1_intro_to_remainder/Test.java
@@ -0,0 +1,13 @@
+package _03_remainder._1_intro_to_remainder;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int test = 0; test>21; test ++);
+		if(test%2 == 0){
+		System.out.print(test + ", ");
+		}
+	}
+
+}
